97a11429009171c4fccee545fb6aebb4a14f5d71
MiguelAstudilloCampo <132482640+MiguelAstudilloCampo@users.noreply.github.com>
2024-02-13 22:15:05 -0500

    Se agrego App Jsx

--- MiniProjecto/src/App.jsx
+++ MiniProjecto/src/App.jsx
@@ -1,12 +1,53 @@
 import React, { useState } from 'react';
+import DataList from './Components/DataList'
+import CreateModal from './Components/Modal'; 
+import EditModal from './Components/Modal Editable';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [selectedData, setSelectedData] = useState(null);
+  const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+
+  const handleCreate = (newData) => {
+    setData([...data, { ...newData, id: Date.now().toString() }]);
+  };
+
+  const handleEdit = (editedData) => {
+    const updatedData = data.map((item) => (item.id === editedData.id ? editedData : item));
+    setData(updatedData);
+  };
+
+  const handleDelete = (id) => {
+    const updatedData = data.filter((item) => item.id !== id);
+    setData(updatedData);
+  };
+
+  const handleEditModalOpen = (selected) => {
+    setSelectedData(selected);
+    setEditModalOpen(true);
+  };
+
   return (
     <div>
-      
+      <h1>Registro de Datos</h1>
+
+      <button onClick={() => setCreateModalOpen(true)}>Crear Registro</button>
+
+      <DataList data={data} onEdit={handleEditModalOpen} onDelete={handleDelete} />
+
+      <CreateModal isOpen={isCreateModalOpen} onClose={() => setCreateModalOpen(false)} onCreate={handleCreate} />
+
+      {selectedData && (
+        <EditModal
+          isOpen={isEditModalOpen}
+          onClose={() => setEditModalOpen(false)}
+          onUpdate={handleEdit}
+          selectedData={selectedData}
+        />
+      )}
     </div>
   );
 };
-
 
 export default App;
